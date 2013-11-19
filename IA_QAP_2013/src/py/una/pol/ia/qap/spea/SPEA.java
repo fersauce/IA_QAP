d00aee10fc6ae8f7968d85e6f3ce94799d09e889
Fernando Saucedo <carlifer.fernando@gmail.com>
2013-11-19 15:31:13 -0300

    #1 Commit Inicial del Proyecto

---
+++ IA_QAP_2013/src/py/una/pol/ia/qap/spea/SPEA.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package py.una.pol.ia.qap.spea;
+
+/**
+ *
+ * @author sauce
+ */
+public class SPEA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
